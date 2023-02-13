410e4ba15d15fd620ea3344c2771434dbae55215
LinkMaq <maqing1213@gmail.com>
2023-02-13 16:03:53 +0800

    1.23.4

--- applications/kubegems/v1.23.0/init.sh
+++ applications/kubegems/v1.23.0/init.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
 export readonly ARCH=${1:-amd64}
 export readonly NAME=${2:-$(basename "${PWD%/*}")}
 export readonly chartVersion=1.23.0
-export readonly appVersion=v1.23.2
+export readonly appVersion=v1.23.4
 
 rm -rf charts/ && mkdir -p charts/
 helm repo add kubegems https://charts.kubegems.io/kubegems
